f6b7db84b1fd1a76d76e0286945a9bad8b86f2c5
Matthew1212 <mefju122@gmail.com>
2017-04-26 14:17:20 +0200

    raport 2

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,6 +8,9 @@ describe('reverse number', function ()
     it('reverse number', function () {
         expect(app.reverseNumber(33334)).to.eql(43333);
     });
+    it('not  number', function () {
+        expect(app.reverseNumber('ddd3')).to.eql(false);
+    });
 });
 
 describe('returnOnlyLetter', function ()
@@ -16,11 +19,22 @@ describe('returnOnlyLetter', function ()
           {
               expect(app.returnOnlyLetter('dfg2')).to.eql('dfg');
         });
+    it('return only  letters', function()
+    {
+        expect(app.returnOnlyLetter('22222')).to.eql('');
+    });
+    it('should return false ', function(){
+        expect(app.returnOnlyLetter(2222)).to.eql(false);
+    })
 });
 
 describe('isEmail', function ()
 {
     it('return true', function () {
         expect(app.isEmail('matthew1212@o2.pl')).to.eql(true);
+    });
+
+    it('return false', function () {
+        expect(app.isEmail('matthew1212@')).to.eql(false);
     });
 });
