429c738ca53558dfa53ddb36f82faac72209c9b3
Matthew1212 <mefju122@gmail.com>
2017-04-26 14:05:44 +0200

    raport 1

--- test/app.spec.js
+++ test/app.spec.js
@@ -3,17 +3,24 @@
 var expect = require('chai').expect;
 var app = require('../app/app');
 
-describe('reverseNumber', function ()
+describe('reverse number', function ()
 {
-
+    it('reverse number', function () {
+        expect(app.reverseNumber(33334)).to.eql(43333);
+    });
 });
 
 describe('returnOnlyLetter', function ()
 {
-
+    it('return only  letters', function()
+          {
+              expect(app.returnOnlyLetter('dfg2')).to.eql('dfg');
+        });
 });
 
 describe('isEmail', function ()
 {
-
+    it('return true', function () {
+        expect(app.isEmail('matthew1212@o2.pl')).to.eql(true);
+    });
 });
